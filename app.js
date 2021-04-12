7d20771bf0d1607e4ea935cf4a654854d62c450e
Manav Garg <58691392+ManavvGarg@users.noreply.github.com>
2021-04-12 17:58:57 +0530

    set up views. Remaining = player and pages

--- app.js
+++ app.js
@@ -15,9 +15,54 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/stats', (req, res) => {
-    res.redirect('https://xenfm.ga/#stats')
+app.get('/english', (req, res) => {
+    res.render('player/views/english')
 })
+
+app.get('/english-lofi', (req, res) => {
+    res.render('player/views/english-lofi')
+})
+
+app.get('/engish-rap', (req, res) => {
+    res.render('player/views/english-rap')
+})
+
+app.get('/english-electro', (req, res) => {
+    res.render('player/views/english-electro')
+})
+
+app.get('/hindi', (req, res) => {
+    res.render('player/views/hindi')
+})
+
+app.get('/hindi-lofi', (req, res) => {
+    res.render('player/views/hindi-lofi')
+})
+
+app.get('/hindi-classics', (req, res) => {
+    res.render('player/views/hindi-classics')
+})
+
+app.get('/gaming', (req, res) => {
+    res.render('player/views/gaming')
+})
+
+app.get('/study', (req, res) => {
+    res.render('player/views/study')
+})
+
+app.get('/japanese', (req, res) => {
+    res.render('player/views/japanese')
+})
+
+app.get('/korean', (req, res) => {
+    res.render('player/views/korean')
+})
+
+app.get('/anime', (req, res) => {
+    res.render('player/views/anime')
+})
+
 
 app.use((req, res) => {
     res.status(404).render('404')
